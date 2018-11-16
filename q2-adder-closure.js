0fc59964b35f0fdf69113030e5523de515870d03
eckmLJE <lucas.eckman@gmail.com>
2018-11-16 16:12:01 -0500

    for submission

--- q2-adder-closure.js
+++ q2-adder-closure.js
@@ -4,3 +4,10 @@ const adder = num => num2 => num + num2;
 console.log("Adder function");
 console.log("Adder(1)(2)", adder(1)(2));
 console.log("Adder(14)(2)", adder(14)(2));
+
+// Output:
+
+// Adder(1)(2)
+//  3
+// Adder(14)(2)
+//  16
